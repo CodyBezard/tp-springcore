1330a02522ed59ef13d59ba4b9449a44c2ddbc18
CodyBezard <cody.bezard@gmail.com>
2024-11-25 11:25:12 +0100

    TP 2 SpringCore

--- src/main/java/fr/diginamic/hello/controleurs/HelloControleur.java
+++ src/main/java/fr/diginamic/hello/controleurs/HelloControleur.java
@@ -1,5 +1,6 @@
 package fr.diginamic.hello.controleurs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/hello")
 public class HelloControleur {
+
+    @Autowired
+    private HelloService helloService;
+
     @GetMapping
     public String sayHello(){
-        return "Hello";
+        return helloService.salutations();
     }
 }
